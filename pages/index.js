c689c68d9efb22713c001ccb893dc671e1a64277
jooprima <jooprima@gmail.com>
2020-12-06 19:49:12 +0700

    setting structure folder

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+class Index extends React.Component{
+
+    render(){
+        return("Hello Index")
+    }
+}
+
+export default Index
---
+++ pages/indonesia.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+class Indonesia extends React.Component{
+
+    render(){
+        return("Hello Indonesia")
+    }
+}
+
+export default Indonesia
